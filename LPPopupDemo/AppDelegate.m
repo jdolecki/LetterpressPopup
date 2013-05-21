8f4ca0b9abfd245c0b12621720ce8d741ce7a3e6
Jesse Squires <jesse.d.squires@gmail.com>
2013-05-20 20:21:10 -0400

    massive refactoring and reorganization.

---
+++ LPPopupDemo/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  LPPopupDemo
+//
+//  Created by Jesse Squires on 5/20/13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    return YES;
+}
+
+@end
---
+++ LPPopupDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LPPopupDemo
+//
+//  Created by Jesse Squires on 5/20/13.
+//  Copyright (c) 2013 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)buttonPressed:(UIButton *)sender;
+
+@end
